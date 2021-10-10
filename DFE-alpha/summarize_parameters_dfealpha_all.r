b8b3149e0a6afd47d6783cbed6d65874c96b788b
Parul Johri <48296846+paruljohri@users.noreply.github.com>
2021-10-09 22:36:04 -0400

    Update summarize_parameters_dfealpha_all.r

--- DFE-alpha/summarize_parameters_dfealpha_all.r
+++ DFE-alpha/summarize_parameters_dfealpha_all.r
@@ -5,7 +5,7 @@
 #This is to summarize the parameters from dfealpha infernece files:
 subfolder <- "_noStacking_Nu0_1"
 
-v_folders <- c("sim4", "sim5", "sim6", "sim7", "sim8", "sim9", "sim1_WF", "sim2_WF", "sim3_WF", "sim13", "sim14", "sim15", "sim16", "sim17", "sim18", "sim19", "sim20", "sim21")
+v_folders <- c("sim13", "sim14", "sim15", "sim16", "sim17", "sim18", "sim19", "sim20", "sim21")
 t_all <- c()
 for (folder in v_folders){
     filename_est <- paste("/home/pjohri1/VirusDfe/DFEalpha", subfolder, "/", folder, "_sel/est_dfe_repall.txt", sep="")
